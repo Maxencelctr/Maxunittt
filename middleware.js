3af52c0c697635d1df84c8f160f6668c0a9a01e6
maxencelacourt24 <maxencelacourt24@gmail.com>
2026-06-07 20:00:03 +0200

    Disable middleware temporairement

--- middleware.js
+++ middleware.js
@@ -1,21 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const url = req.nextUrl.pathname
-
-  if (url === '/admin/login') {
-    return NextResponse.next()
-  }
-
-  const cookies = req.cookies.getAll()
-  const isConnected = cookies.some(c =>
-    c.name.includes('supabase') || c.name.includes('sb-')
-  )
-
-  if (!isConnected) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
   return NextResponse.next()
 }
 
