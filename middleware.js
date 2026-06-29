48a74071687654e13ca4666497e4f99419fdbc3f
maxencelacourt24 <maxencelacourt24@gmail.com>
2026-06-29 16:18:12 +0200

    Securiser admin avec variable env

--- middleware.js
+++ middleware.js
@@ -1,6 +1,25 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
+  const url = req.nextUrl.pathname
+  const secret = req.nextUrl.searchParams.get('secret')
+
+  if (url === '/admin/login') {
+    if (secret !== process.env.ADMIN_SECRET) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+    return NextResponse.next()
+  }
+
+  const cookies = req.cookies.getAll()
+  const isConnected = cookies.some(c =>
+    c.name.includes('supabase') || c.name.includes('sb-')
+  )
+
+  if (!isConnected) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
   return NextResponse.next()
 }
 
